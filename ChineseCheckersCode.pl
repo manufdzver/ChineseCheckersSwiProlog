72e0c1144a1e63331c6ef0daecfae7510f3ba717
SantiagoBorobia <34692379+SantiagoBorobia@users.noreply.github.com>
2020-05-03 12:55:48 -0500

    Add files via upload

--- ChineseCheckersCode.pl
+++ ChineseCheckersCode.pl
@@ -1,3 +1,4 @@
+
 %.................TABLERO INICIAL.............................
 %
 % Cada posicion se compone de dos coordenadas (X,Y).
@@ -20,28 +21,28 @@ computadoraR( 3 , 14 ).
 computadoraR( 4 , 14 ).
 
 :- dynamic jugadorA/ 2.
-jugadorA( -4 , 13 ).
 jugadorA( -3 , 13 ).
 jugadorA( -2 , 13 ).
 jugadorA( -1 , 13 ).
-jugadorA( -3 , 12 ).
+jugadorA( 0 , 13 ).
 jugadorA( -2 , 12 ).
 jugadorA( -1 , 12 ).
-jugadorA( -2 , 11 ).
+jugadorA( 0 , 12 ).
 jugadorA( -1 , 11 ).
-jugadorA( -1 , 10 ).
+jugadorA( 0 , 11 ).
+jugadorA( 0 , 10 ).
 
 :- dynamic jugadorB/ 2.
-jugadorB( -1 , 8).
-jugadorB( -2 , 7 ).
+jugadorB( 0 , 8).
+jugadorB( 0 , 7 ).
 jugadorB( -1 , 7 ).
+jugadorB( 0 , 6 ).
 jugadorB( -1 , 6 ).
 jugadorB( -2 , 6 ).
-jugadorB( -3 , 6 ).
+jugadorB( 0 , 5 ).
 jugadorB( -1 , 5 ).
 jugadorB( -2 , 5 ).
 jugadorB( -3 , 5 ).
-jugadorB( -4 , 5 ).
 
 :- dynamic jugadorV/ 2.
 jugadorV( 1 , 1 ).
@@ -68,7 +69,7 @@ jugadorZ( 9 , 7 ).
 jugadorZ( 9 , 8 ).
 
 :- dynamic jugadorN/ 2.
-jugadorN( 6 , 13 ).
+%jugadorN( 6 , 13 ).
 jugadorN( 7 , 13 ).
 jugadorN( 8 , 13 ).
 jugadorN( 9 , 13 ).
@@ -96,88 +97,93 @@ casilla(X , Y):-
     X =< ( 9 - G ).
 
 casilla2(X , Y):-
-    G is abs( Y - 9 ),
+    G is abs( Y - 10 ),
     Y > 4,
     Y =< 13,
-    (X < 0, X >= (0-G);
-    X >(9-G), X =< 9).
+    (   (X =< 0, X >= (0-G),!);
+        (X >(9-G), X =< 9)).
 
 
 % -- Checar si dos casillas son diferentes
 %
 diferentes(X1 , Y1 , X2 , Y2):-
-    X1 \== X2;
-    Y1 \== Y2.
+    (X1 \== X2,!;
+    Y1 \== Y2).
 
 % -- Checar si un dos casillas son adjuntas
-%
-% El primero siempre falla si algun miembro no pertenece a las casillas
-% validas del tablero y luego checamos los casos que si son adjuntos.
-%
-adjuntos( X1 , Y1 , X2 , Y2 ):-
-    (not(casilla( X1 , Y1 ));
-    not(casilla( X2 , Y2 ))),
-    !,
-    fail.
+
 
 % Adjuntos horizontales
 %
 adjuntos( X1 , Y1 , X2 , Y2 ):-
+    (   (casilla( X1 , Y1 ), casilla( X2 , Y2 ));
+        (casilla2( X1 , Y1 ), casilla2( X2 , Y2 ))),
     Y1 == Y2,
     G is abs( X1 - X2 ),
-    G  ==  1.
+    G  ==  1, !.
 
 % Adjuntos verticales
 %
 adjuntos( X1 , Y1 , X2 , Y2 ):-
+    (   (casilla( X1 , Y1 ), casilla( X2 , Y2 ));
+        (casilla2( X1 , Y1 ), casilla2( X2 , Y2 ))),
     X1 == X2,
     G is abs( Y1 - Y2 ),
-    G  ==  1 .
+    G  ==  1, !.
 
-% Adjuntos diagonales en la mitad inferior del tablero (Checamos
-% movimientos para arriba y abajo)
-%
 adjuntos( X1 , Y1 , X2 , Y2 ):-
-    Y1  <  9,
-    W is Y2 - Y1,
-    W  ==  1,
-    Z is X2 - X1,
-    Z  ==  1.
-adjuntos( X1 , Y1 , X2 , Y2 ):-
-    Y1  =<  9,
-    W is Y2 - Y1,
-    W  ==  - 1,
-    Z is X2 - X1,
-    Z  ==  - 1.
+    (   (casilla( X1 , Y1 ), casilla( X2 , Y2 ));
+        (casilla2( X1 , Y1 ), casilla2( X2 , Y2 ))),
+    H is abs(X1-X2),
+    H == 1,
+    G is abs( Y1 - Y2 ),
+    G  ==  1, !.
 
-% Adjuntos diagonales en la mitad superior del tablero
-%
+% Salida/Entrada de X negativas y Salida/Entrada de excesos de X.
+% Adjuntos horizontales
 adjuntos( X1 , Y1 , X2 , Y2 ):-
-    Y1  >= 9,
-    W is Y2 - Y1,
-    W  ==  1,
-    Z is X1 - X2,
-    Z  ==  1 .
+    (   (casilla( X1 , Y1 ), casilla2( X2 , Y2 ));
+        (casilla2( X1 , Y1 ), casilla( X2 , Y2 ))   ),
+    Y1 == Y2,
+    G is abs( X1 - X2 ),
+    G  ==  1, !.
+
+%Adjuntos Verticales
 adjuntos( X1 , Y1 , X2 , Y2 ):-
-    Y1  >  9,
-    W is Y2 - Y1,
-    W  ==  - 1,
-    Z is X1 - X2,
-    Z  ==  - 1.
+    (   (casilla( X1 , Y1 ), casilla2( X2 , Y2 ));
+        (casilla2( X1 , Y1 ), casilla( X2 , Y2 ))   ),
+    X1 == X2,
+    G is abs( Y1 - Y2 ),
+    G  ==  1, !.
+
+adjuntos( X1 , Y1 , X2 , Y2 ):-
+    (   (casilla( X1 , Y1 ), casilla2( X2 , Y2 ));
+        (casilla2( X1 , Y1 ), casilla( X2 , Y2 ))   ),
+    H is abs(X1-X2),
+    H == 1,
+    G is abs( Y1 - Y2 ),
+    G  ==  1, !.
+
+
+
 
 %.................CONDICIONES DE LA CASILLA......................
 %
 % Una casilla puede estar libre u ocupada
 %
 libre( X , Y ):-
-    casilla( X , Y ),
-    not(jugadorV( X , Y )),
-    not(computadoraR( X , Y )).
+    ((casilla( X , Y ),!);casilla2(X,Y)),
+    not(ocupada(X,Y)).
 
 ocupada( X , Y ):-
-    casilla( X , Y ),
-    (jugadorV( X , Y );
-    computadoraR( X , Y )).
+    ((casilla( X , Y ),!);casilla2(X,Y)),
+    (   jugadorV( X, Y );
+        jugadorN( X, Y );
+        jugadorZ( X, Y );
+        jugadorB( X, Y );
+        jugadorA( X, Y );
+    computadoraR( X, Y )
+    ).
 
 
 %.................SALTOS A CASILLAS..............................
@@ -191,7 +197,7 @@ ocupada( X , Y ):-
 saltoFact( X1 , Y1 , X2 , Y2 ):-
     diferentes( X1 , Y1 , X2 , Y2 ),
     libre( X2 , Y2 ),
-    Diff is X1 - X2,
+    Diff is abs(X1 - X2),
     Diff == 2,
     Xi is X1 - 1,
     Y1 == Y2,
@@ -203,7 +209,7 @@ saltoFact( X1 , Y1 , X2 , Y2 ):-
 saltoFact( X1 , Y1 , X2 , Y2 ):-
     diferentes( X1 , Y1 , X2 , Y2 ),
     libre( X2 , Y2 ),
-    Diff is X2 - X1 ,
+    Diff is abs(X2 - X1) ,
     Diff == 2,
     Xi is X1 + 1,
     Y1 == Y2,
@@ -231,8 +237,8 @@ saltoFact( X1 , Y1 , X2 , Y2 ):-
 saltoFact( X1 , Y1 , X2 , Y2 ):-
     diferentes( X1 , Y1 , X2 , Y2 ),
     libre( X2 , Y2 ),
-    Y1 < 9,
-    Y2 =< 9,
+    Y1 =< 9,
+    Y2 < 9,
     DiffX is X1 - X2,
     DiffY is Y1 - Y2,
     ( DiffX == 2 ; DiffX == 0 ),
@@ -248,8 +254,8 @@ saltoFact( X1 , Y1 , X2 , Y2 ):-
 saltoFact( X1 , Y1 , X2 , Y2 ):-
     diferentes( X1 , Y1 , X2 , Y2 ),
     libre( X2 , Y2 ),
-    Y1 > 9,
-    Y2 >= 9,
+    Y1 >= 9,
+    Y2 > 9,
     DiffX is X1 - X2,
     DiffY is Y2 - Y1,
     ( DiffX == 2 ; DiffX == 0 ),
@@ -324,6 +330,120 @@ saltoFact( X1 , Y1 , X2 , Y2 ):-
     ocupada( X1 , Yi ),
     !.
 
+%.................SALTOS MÚLTIPLES..............................
+%
+
+%------ Saltos dobles.
+
+% Primer salto horizontal.
+salto2Fact(X1,Y1,X2,Y2):- 
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        (
+            (X4 is X1 + 2, saltoFact(X1,Y1,X4,Y1),saltoFact(X4,Y1,X2,Y2));
+            (X3 is X1 - 2, saltoFact(X1,Y1,X3,Y1),saltoFact(X3,Y1,X2,Y2))
+        )
+    ),!.
+
+%- BAJANDO (Rojo)
+
+% Primer salto vertical.
+salto2Fact(X1,Y1,X2,Y2):-
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        Y1 > 10,
+        Y3 is Y1 - 2,
+        (
+            (saltoFact(X1,Y1,X1,Y3), saltoFact(X1,Y3,X2,Y2));
+            (X3 is X1 + 2, saltoFact(X1,Y1,X3,Y3), saltoFact(X3,Y3,X2,Y2))
+        )
+    ), !.
+
+salto2Fact(X1,Y1,X2,Y2):-
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        Y1 =:= 10,
+        Y3 is Y1 - 2,
+        (
+            (X3 is X1 - 1, saltoFact(X1,Y1,X3,Y3),saltoFact(X3,Y3,X2,Y2));
+            (X4 is X1 + 1, saltoFact(X1,Y1,X4,Y3),saltoFact(X4,Y3,X2,Y2))
+         )
+    ), !.
+
+salto2Fact(X1,Y1,X2,Y2):-
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        Y1=<9,
+        Y3 is Y1 - 2,
+        (
+            (saltoFact(X1,Y1,X1,Y3),saltoFact(X1,Y3,X2,Y2));
+            (X3 is X1 - 2, saltoFact(X1,Y1,X3,Y3),saltoFact(X3,Y3,X2,Y2))
+        )
+    ), !.
+
+
+%------ Saltos de n espacios.
+%Solo se tiene que cambiar saltoFact por salto2Fact en el metodo de arriba.
+
+%- SUBIENDO (Verde)
+% Primer salto vertical.
+salto2Fact(X1,Y1,X2,Y2):-
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        Y1 >= 9,
+        Y3 is Y1 + 2,
+        (
+            (saltoFact(X1,Y1,X1,Y3), saltoFact(X1,Y3,X2,Y2));
+            (X3 is X1 - 2, saltoFact(X1,Y1,X3,Y3), saltoFact(X3,Y3,X2,Y2))
+        )
+    ), !.
+
+salto2Fact(X1,Y1,X2,Y2):-
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        Y1 =:= 8,
+        Y3 is Y1 + 2,
+        (
+            (X3 is X1 - 1, saltoFact(X1,Y1,X3,Y3),saltoFact(X3,Y3,X2,Y2));
+            (X4 is X1 + 1, saltoFact(X1,Y1,X4,Y3),saltoFact(X4,Y3,X2,Y2))
+         )
+    ), !.
+
+salto2Fact(X1,Y1,X2,Y2):-
+    %saltoFact(X1,Y1,X2,Y2);
+    (
+        Y1<8,
+        Y3 is Y1 + 2,
+        (
+            (saltoFact(X1,Y1,X1,Y3),saltoFact(X1,Y3,X2,Y2));
+            (X3 is X1 + 2, saltoFact(X1,Y1,X3,Y3),saltoFact(X3,Y3,X2,Y2))
+        )
+    ), !.
+
+% Subiendo (Verde)
+salto3Fact(X1,Y1,X2,Y2):-
+    jugadorV(X1,Y1),
+    findall([ Xx , Yx ],
+            (numlist( 1 , 9 , DX ),numlist(Y1,17 , DY ), miembro( Yx , DY ), miembro( Xx , DX ),libre(Xx,Yx), salto2Fact( X1 , Y1 , Xx , Yx )),
+            M),
+    salto3FactPrueba(M,X2,Y2),!.
+
+% Bajando (Rojo)
+salto3Fact(X1,Y1,X2,Y2):-
+    computadoraR(X1,Y1),
+    findall([ Xx , Yx ],
+            (numlist( 1 , 9 , DX ),numlist( 1 , Y1 , DY ), miembro( Yx , DY ), miembro( Xx , DX ),libre(Xx,Yx), salto2Fact( X1 , Y1 , Xx , Yx )),
+            M),
+    salto3FactPrueba(M,X2,Y2),!.
+
+salto3FactPrueba([L|_],X2,Y2):-
+    nth1(1,L,X1),
+    nth1(2,L,Y1),
+    saltoFact(X1,Y1,X2,Y2),
+    !.
+salto3FactPrueba([_|Q],X2,Y2):-
+    salto3FactPrueba(Q,X2,Y2),!.
+
 
 %.................PASOS A CASILLAS..............................
 %
@@ -338,10 +458,13 @@ pasoFact( X1 , Y1 , X2 , Y2 ):-
 % a el.
 %
 movVal( X1 , Y1 , X2 , Y2 ):-
-    pasoFact( X1 , Y1 , X2 , Y2 ),
-    !.
-movVal( X1 , Y1 , X2 , Y2 ):-
-    saltoFact( X1 , Y1 , X2 , Y2 ),
+    (pasoFact( X1 , Y1 , X2 , Y2 );
+    movVal2(X1 , Y1 , X2 , Y2 )),!.
+
+movVal2( X1 , Y1 , X2 , Y2 ):-
+    (saltoFact(X1 , Y1 , X2 , Y2);
+    salto2Fact( X1 , Y1 , X2 , Y2 );
+    salto3Fact(X1, Y1, X2, Y2)),
     ! .
 
 
@@ -358,7 +481,8 @@ ganoJugadorV:-
     jugadorV( 1 , 14 ),
     jugadorV( 2 , 14 ),
     jugadorV( 3 , 14 ),
-    jugadorV( 4 , 14 ).
+    jugadorV( 4 , 14 ),
+    writeln("¡Gana el jugador VERDE!").
 
 ganoComputadoraR:-
     computadoraR( 1 , 1 ),
@@ -370,7 +494,8 @@ ganoComputadoraR:-
     computadoraR( 1 , 4 ),
     computadoraR( 2 , 4 ),
     computadoraR( 3 , 4 ),
-    computadoraR( 4 , 4 ).
+    computadoraR( 4 , 4 ),
+    writeln("¡Gana el jugador ROJO!").
 
 
 
@@ -548,17 +673,16 @@ heurJugadorVAvanza2([ T | Q ], Val ):-
 %...................POS. ACCESIBLES............................
 %
 
-
 % Devuelve la lista de espacios disponibles
 
 espaciosDisponiblesComputadoraR( X , Y , M ):-
     findall([ X2 , Y2 ],
-            (numlist( 1 , 9 , DX ),numlist( 1 , Y , DY ), miembro( X2 , DX ), miembro( Y2 , DY ), movVal( X , Y , X2 , Y2 )),
-            M).
+            (numlist( 1 , 9 , DX ),numlist( 1 , Y , DY ), miembro( Y2 , DY ), miembro( X2 , DX ),libre(X2,Y2), movVal( X , Y , X2 , Y2 )),
+            M) .
 
 espaciosDisponiblesJugadorV( X , Y , M ):-
     findall([ X2 , Y2 ],
-            (numlist( 1 , 9 , DX ), numlist( Y , 17 , DY ), miembro( X2 , DX ), miembro( Y2 , DY ) , movVal( X , Y , X2 , Y2 )),
+            (numlist( 1 , 9 , DX ), numlist( Y , 17 , DY ), miembro( X2 , DX ), miembro( Y2 , DY ) , libre(X2,Y2), movVal( X , Y , X2 , Y2 )),
             M) .
 
 % Turno 1 juega la compu, turno 2 juega el jugadorV
@@ -623,7 +747,7 @@ succ([ Compu , Player ], Turno , PosList , IA ):-
 	IA == 1 ,
         Turno == 1 ,
 	succCalc( Compu , Compu , Temp , Turno , IA ),
-        distribucionJugadorV(Temp , Player , PosList ). %, writeln (PosList).
+        distribucionJugadorV(Temp , Player , PosList ),!. %, writeln (PosList).
 
 succ([ Compu , _ ], Turno , PosList , IA ):-
 	IA == 1 ,
@@ -760,27 +884,27 @@ alphabeta([ E | L ], 2 , P , A , B , MejorMov , V , IA ):-
 % Draw o si el cuadro existe, 1 para la CPU, 2 para el jugador
 draw( A , B ):-
     computadoraR( A , B ),
-    write('R'),
+    ansi_format([bold,fg(red)], 'R~w', [""]),
     !.
 draw( A , B ):-
     jugadorA( A , B ),
-    write('A'),
+    ansi_format([bold,fg(yellow)], 'A~w', [""]),
     !.
 draw( A , B ):-
     jugadorB( A , B ),
-    write('B'),
+    ansi_format([bold,fg(magenta)], 'B~w', [""]),
     !.
 draw( A , B ):-
     jugadorV( A , B ),
-    write('V'),
+    ansi_format([bold,fg(green)], 'V~w', [""]),
     !.
 draw( A , B ):-
     jugadorZ( A , B ),
-    write('Z'),
+    ansi_format([bold,fg(blue)], 'Z~w', [""]),
     !.
 draw( A , B ):-
     jugadorN( A , B ),
-    write('N'),
+    ansi_format([bold,fg(black)], 'N~w', [""]),
     !.
 draw( A , B ):-
     casilla( A , B ),
@@ -827,31 +951,31 @@ writeln(''),
 write('             '),draw(1,14),write(' '),draw(2,14),write(' '),draw(3,14),write(' '),draw(4,14),write('     ').
 drawLinea13:-
 writeln(''),
-write('    '),draw(-4,13),write(' '), draw(-3,13),write(' '), draw(-2,13),write(' '), draw(-1,13),write(' '), draw(1,13),write(' '), draw(2,13),write(' '), draw(3,13),write(' '), draw(4,13),write(' '),draw(5,13), write(' '), draw(6,13), write(' '), draw(7,13), write(' '), draw(8,13), write(' '), draw(9,13),write('    ').
+write('    '),draw(-3,13),write(' '), draw(-2,13),write(' '), draw(-1,13),write(' '), draw(0,13),write(' '), draw(1,13),write(' '), draw(2,13),write(' '), draw(3,13),write(' '), draw(4,13),write(' '),draw(5,13), write(' '), draw(6,13), write(' '), draw(7,13), write(' '), draw(8,13), write(' '), draw(9,13),write('    ').
 drawLinea12:-
 writeln(''),
-write('     '), draw(-3,12), write( ' ' ), draw(-2,12), write( ' ' ), draw(-1,12), write( ' ' ), draw(1,12), write( ' ' ), draw(2,12), write( ' ' ), draw(3,12), write( ' ' ), draw(4,12), write( ' ' ), draw(5,12), write( ' ' ), draw(6,12), write(' '), draw(7,12), write(' '), draw(8,12), write(' '), draw(9,12),write( '   ' ).
+write('     '), draw(-2,12), write( ' ' ), draw(-1,12), write( ' ' ), draw(0,12), write( ' ' ), draw(1,12), write( ' ' ), draw(2,12), write( ' ' ), draw(3,12), write( ' ' ), draw(4,12), write( ' ' ), draw(5,12), write( ' ' ), draw(6,12), write(' '), draw(7,12), write(' '), draw(8,12), write(' '), draw(9,12),write( '   ' ).
 drawLinea11:-
 writeln(''),
-write('      '),draw(-2,11),write(' '),draw(-1,11),write(' '),draw(1,11),write(' '),draw(2,11),write(' '),draw(3,11),write(' '),draw(4,11),write(' '),draw(5,11),write(' '),draw(6,11),write(' '),draw(7,11), write(' '), draw(8,11), write(' '), draw(9,11),write('  ').
+write('      '),draw(-1,11),write(' '),draw(0,11),write(' '),draw(1,11),write(' '),draw(2,11),write(' '),draw(3,11),write(' '),draw(4,11),write(' '),draw(5,11),write(' '),draw(6,11),write(' '),draw(7,11), write(' '), draw(8,11), write(' '), draw(9,11),write('  ').
 drawLinea10:-
 writeln(''),
-write('       '),draw(-1,10),write(' '),draw(1,10),write(' '),draw(2,10),write(' '),draw(3,10),write(' '),draw(4,10),write(' '),draw(5,10),write(' '),draw(6,10),write(' '),draw(7,10),write(' '),draw(8,10), write(' '), draw(9,10),write('  ').
+write('       '),draw(0,10),write(' '),draw(1,10),write(' '),draw(2,10),write(' '),draw(3,10),write(' '),draw(4,10),write(' '),draw(5,10),write(' '),draw(6,10),write(' '),draw(7,10),write(' '),draw(8,10), write(' '), draw(9,10),write('  ').
 drawLinea9:-
 writeln(''),
 write('        '),draw(1,9),write(' '),draw(2,9),write(' '),draw(3,9),write(' '),draw(4,9),write(' '),draw(5,9),write(' '),draw(6,9),write(' '),draw(7,9),write(' '),draw(8,9), write(' '), draw(9,9), write(' ').
 drawLinea8:-
 writeln(''),
-write('       '),draw(-1,8),write(' '),draw(1,8),write(' '),draw(2,8),write(' '),draw(3,8),write(' '),draw(4,8),write(' '),draw(5,8),write(' '),draw(6,8),write(' '),draw(7,8),write(' '),draw(8,8), write(' '), draw(9,8),write('  ').
+write('       '),draw(0,8),write(' '),draw(1,8),write(' '),draw(2,8),write(' '),draw(3,8),write(' '),draw(4,8),write(' '),draw(5,8),write(' '),draw(6,8),write(' '),draw(7,8),write(' '),draw(8,8), write(' '), draw(9,8),write('  ').
 drawLinea7:-
 writeln(''),
-write('      '),draw(-2,7),write(' '),draw(-1,7),write(' '),draw(1,7),write(' '),draw(2,7),write(' '),draw(3,7),write(' '),draw(4,7),write(' '),draw(5,7),write(' '),draw(6,7),write(' '),draw(7,7), write(' '), draw(8,7), write(' '), draw(9,7),write('  ').
+write('      '),draw(-1,7),write(' '),draw(0,7),write(' '),draw(1,7),write(' '),draw(2,7),write(' '),draw(3,7),write(' '),draw(4,7),write(' '),draw(5,7),write(' '),draw(6,7),write(' '),draw(7,7), write(' '), draw(8,7), write(' '), draw(9,7),write('  ').
 drawLinea6:-
 writeln(''),
-write('     '), draw(-3,6), write( ' ' ), draw(-2,6), write( ' ' ), draw(-1,6), write( ' ' ), draw(1,6), write( ' ' ), draw(2,6), write( ' ' ), draw(3,6), write( ' ' ), draw(4,6), write( ' ' ), draw(5,6), write( ' ' ), draw(6,6), write(' '), draw(7,6), write(' '), draw(8,6), write(' '), draw(9,6),write( '   ' ).
+write('     '), draw(-2,6), write( ' ' ), draw(-1,6), write( ' ' ), draw(0,6), write( ' ' ), draw(1,6), write( ' ' ), draw(2,6), write( ' ' ), draw(3,6), write( ' ' ), draw(4,6), write( ' ' ), draw(5,6), write( ' ' ), draw(6,6), write(' '), draw(7,6), write(' '), draw(8,6), write(' '), draw(9,6),write( '   ' ).
 drawLinea5:-
 writeln(''),
-write('    '),draw(-4,5),write(' '), draw(-3,5),write(' '), draw(-2,5),write(' '), draw(-1,5),write(' '), draw(1,5),write(' '), draw(2,5),write(' '), draw(3,5),write(' '), draw(4,5),write(' '),draw(5,5), write(' '), draw(6,5), write(' '), draw(7,5), write(' '), draw(8,5), write(' '), draw(9,5),write('    ').
+write('    '),draw(-3,5),write(' '), draw(-2,5),write(' '), draw(-1,5),write(' '), draw(0,5),write(' '), draw(1,5),write(' '), draw(2,5),write(' '), draw(3,5),write(' '), draw(4,5),write(' '),draw(5,5), write(' '), draw(6,5), write(' '), draw(7,5), write(' '), draw(8,5), write(' '), draw(9,5),write('    ').
 
 drawLinea1:-
 writeln(''),
@@ -872,7 +996,7 @@ write('             '),draw(1,4),write(' '),draw(2,4),write(' '),draw(3,4),write
 
 jugarComputadoraR( IA ):-
     tableroPos( Pos ),
-    minimaxab( Pos , 1 , 3 , MejorMov , _ , IA ),
+    minimaxab( Pos , 1 , 2 , MejorMov , _ , IA ),
     !,
     retractall(computadoraR( _ , _ )),
     nth1( 1 , MejorMov , Mejor ),
@@ -902,6 +1026,9 @@ jugarJugadorV( IA ):-
 	),
 	drawTablero,
         ! .
+
+
+
 
 % -- Mover JugadorVerde
 %
@@ -935,21 +1062,12 @@ moverJugadorB( X1 , Y1 , X2 , Y2 ):-
     assert(jugadorB( X2 , Y2 )),
     !.
 
-moverJugadorA( X1 , Y1 , X2 , Y2 ):-
-    jugadorB( X1 , Y1 ),
-    movVal( X1 , Y1 , X2 , Y2 ),
-    retract(jugadorB( X1 , Y1 )),
-    assert(jugadorB( X2 , Y2 )),
-    !.
-
 moverJugadorN( X1 , Y1 , X2 , Y2 ):-
     jugadorB( X1 , Y1 ),
     movVal( X1 , Y1 , X2 , Y2 ),
     retract(jugadorB( X1 , Y1 )),
     assert(jugadorB( X2 , Y2 )),
     !.
-
-
 
 
 
@@ -967,6 +1085,7 @@ newGame:-
     retractall(jugadorB( _ , _ )),
     retractall(jugadorZ( _ , _ )),
     retractall(jugadorN( _ , _ )),
+    retractall(jugadorA( _ , _ )),
 
     assert(computadoraR( 1 , 17 )),
     assert(computadoraR( 1 , 16 )),
@@ -990,27 +1109,27 @@ newGame:-
     assert(jugadorV( 3 , 4 )),
     assert(jugadorV( 4 , 4 )),
 
-    assert(jugadorA( -4 , 13 )),
     assert(jugadorA( -3 , 13 )),
     assert(jugadorA( -2 , 13 )),
     assert(jugadorA( -1 , 13 )),
-    assert(jugadorA( -3 , 12 )),
+    assert(jugadorA( 0 , 13 )),
     assert(jugadorA( -2 , 12 )),
     assert(jugadorA( -1 , 12 )),
-    assert(jugadorA( -2 , 11 )),
+    assert(jugadorA( 0 , 12 )),
     assert(jugadorA( -1 , 11 )),
-    assert(jugadorA( -1 , 10 )),
+    assert(jugadorA( 0 , 11 )),
+    assert(jugadorA( 0 , 10 )),
 
-    assert(jugadorB( -1 , 8 )),
-    assert(jugadorB( -2 , 7 )),
+    assert(jugadorB( 0 , 8 )),
+    assert(jugadorB( 0 , 7 )),
     assert(jugadorB( -1 , 7 )),
+    assert(jugadorB( 0 , 6 )),
     assert(jugadorB( -1 , 6 )),
     assert(jugadorB( -2 , 6 )),
-    assert(jugadorB( -3 , 6 )),
+    assert(jugadorB( 0 , 5 )),
     assert(jugadorB( -1 , 5 )),
     assert(jugadorB( -2 , 5 )),
     assert(jugadorB( -3 , 5 )),
-    assert(jugadorB( -4 , 5 )),
 
     assert(jugadorZ( 6 , 5 )),
     assert(jugadorZ( 7 , 5 )),
@@ -1036,8 +1155,19 @@ newGame:-
 
     drawTablero.
 
+menu:-
+    writeln("DAMAS CHINAS"),
+    writeln("Selecciona la opción deseada para jugar"),
+    writeln("1. 1H - 1C"),
+    writeln("2. 3H - 1C"),
+    writeln("3. 5H - 1C"),
+    writeln("4. Salir"),
+    read(X),
+    iniciarJuego(X).
 
-
-
+iniciarJuego(X):-
+    X==1,
+    writeln("Modo 1 jugador vs computadora"),
+    newGame.
 
 
